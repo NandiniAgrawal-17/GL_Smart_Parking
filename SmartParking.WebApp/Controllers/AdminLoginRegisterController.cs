f7ac7943045ea997ce665f71102f8d1fd7809764
PawanSharmaGL <pawan.sharma2@globallogic.com>
2023-05-26 10:04:12 +0530

    first commit

---
+++ SmartParking.WebApp/Controllers/AdminLoginRegisterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartParking.WebApp.Controllers
+{
+    public class AdminLoginRegisterController : Controller
+    {
+        public IActionResult AdminLogin()
+        {
+            return View();
+        }
+        public IActionResult AdminRegister()
+        {
+            return View();
+        }
+    }
+}
--- SmartParking.WebApp/Program.cs
+++ SmartParking.WebApp/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=AdminLoginRegister}/{action=AdminRegister}/{id?}");
 
 app.Run();
